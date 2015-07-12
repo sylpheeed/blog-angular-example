c188702c97b1ea4e5fedb38dab42887e3522ef07
sylpheeed <sylpheed@mail.ru>
2015-07-13 00:25:10 +0300

    added make-routes to project;

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,28 +1,34 @@
-//import makeRoutes from 'make-routes';
+import makeRoutes from 'make-routes';
 
 function routerConfig($routeProvider, $locationProvider) {
   'ngInject';
+
+
+  makeRoutes.init({
+    root: {path: '/', to: '/app/controllers/main/main.html', _extra: 'MainController'},
+    auth: {
+      sign_in: {to: '/app/controllers/auth/sign_in/sign_in.html', _extra: 'SignInController'},
+      sign_up: {to: '/app/controllers/auth/sign_in/sign_up.html', _extra: 'SignUpController'}
+    }
+  });
 
   $locationProvider.html5Mode({
     enabled: true,
     requireBase: false
   });
-  $routeProvider
-    .when('/', {
-      templateUrl: '/app/controllers/main/main.html',
-      controller: 'MainController'
-    })
-    .when('/auth/sign_in', {
-      templateUrl: '/app/controllers/auth/sign_in/sign_in.html',
-      controller: 'SignInController'
-    })
-    .when('/auth/sign_up', {
-      templateUrl: '/app/controllers/auth/sign_up/sign_up.html',
-      controller: 'SignInController'
-    })
-    .otherwise({
-      redirectTo: '/'
+
+  let routes = makeRoutes.all();
+  for (let route in routes) {
+    let current = routes[route];
+    $routeProvider.when(current.path, {
+      templateUrl: current.to,
+      controller: current._extra
     });
+  }
+
+  $routeProvider.otherwise({
+    redirectTo: '/'
+  });
 }
 
 export default routerConfig;
